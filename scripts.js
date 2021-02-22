69bbfe87fe4debcf6d112971755bbfae03e92db6
Artem Grinko <asgrinko@alfabank.ru>
2021-02-22 18:37:02 +0300

    Перемешивание карточек, цифры заменены на картинки

--- scripts.js
+++ scripts.js
@@ -4,36 +4,49 @@ function init() {
     let openedCards = [];
     console.log("Скрипты подключены");
     let cards = document.getElementsByTagName("td");
+    shuffleCards(cards);
     for (let card of cards) {
         card.onclick = () => onCardClick(card);
     }
     function onCardClick(card) {
+        if (openedCards.length >= 2) return;
         if (card.classList.contains('open')) {
             card.classList.remove('open');
         } else {
             card.classList.add('open');
             openedCards.push(card);
         }
-        console.log(openedCards);
         setTimeout(() => {
             if (openedCards.length >= 2) {
                 let isSame = isSameCards(openedCards[0], openedCards[1]);
                 if (isSame) {
                     openedCards[0].style["visibility"] = "hidden";
                     openedCards[1].style["visibility"] = "hidden";
-                } else {
-                    openedCards[0].classList.remove('open');
-                    openedCards[1].classList.remove('open');
-                    openedCards = [];
                 }
+                for (let card of openedCards) {
+                    card.classList.remove('open');
+                }
+                openedCards = [];
             }
-        },1000);
-        
+        }, 1500);
+
     }
 
     function isSameCards(card1, card2) {
-        let value1 = card1.children[1].innerText;
-        let value2 = card2.children[1].innerText;
+        let value1 = card1.children[1].children[0].src;
+        let value2 = card2.children[1].children[0].src;
         return value1 === value2;
+    }
+
+    function shuffleCards(cards) {
+        for (let i = 0; i < 500; i++) {
+            let cardIndex1 = Math.round(Math.random() * (cards.length - 1));
+            let cardIndex2 = Math.round(Math.random() * (cards.length - 1));
+            let card1 = cards[cardIndex1];
+            let card2 = cards[cardIndex2];
+            let imgTmp = card1.children[1].children[0].src;
+            card1.children[1].children[0].src = card2.children[1].children[0].src;
+            card2.children[1].children[0].src = imgTmp;
+        }
     }
 }
