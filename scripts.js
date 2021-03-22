56d3e164365164850ec114ce47388a481190d397
Artem Grinko <asgrinko@alfabank.ru>
2021-03-22 16:09:16 +0300

    просмотр карт, экран победы

--- scripts.js
+++ scripts.js
@@ -2,9 +2,11 @@ let openedCards = [];
 let initTime = 60;
 let time = 0;
 let score = 0;
+let cards;
 
 window.onload = function() {
     console.log("Скрипты подключены");
+    init();
     initUI();
 }
 
@@ -14,29 +16,61 @@ function initUI() {
     const timerTag = document.getElementById("timer");
     const scoreTag = document.getElementById("score");
     startButton.onclick = () => {
-        init();
+        const container = document.getElementsByClassName("game-container")[0];
+        container.style['background-image'] = "url(images/wood.jpg)";
+        shuffleCards(cards);
         time = initTime;
         score = 0;
         menu.style["display"] = "none";
         timerTag.innerText = time;
         scoreTag.innerText = score;
-        let interval = setInterval(() => {
-            time--;
-            if (time < 0) {
-                clearInterval(interval);
-                menu.style["display"] = "flex";
-            } else {
-                timerTag.innerText = time;
-                scoreTag.innerText = score;
-            }
-        }, 1000);
+        showAllCards();
+        setTimeout(() => {
+            hideAllCards();
+            let interval = setInterval(() => {
+                time--;
+                if (time < 0) {
+                    clearInterval(interval);
+                    menu.style["display"] = "flex";
+                } else {
+                    timerTag.innerText = time;
+                    scoreTag.innerText = score;
+                }
+            }, 1000);
+        }, 5000);     
     }
+}
 
+function winner() {
+    const container = document.getElementsByClassName("game-container")[0];
+    container.style['background-image'] = "url(winner.gif)";
+}
+
+function checkWinner() {
+    for (let card of cards) {
+        if (card.style["visibility"] !== 'hidden') {
+            return false;
+        }
+    }
+    return true;
+}
+
+function showAllCards() {
+    document.getElementById("blocker").style["display"] = "block";
+    for (let card of cards) {
+        card.classList.add('open')
+    }
+}
+
+function hideAllCards() {
+    document.getElementById("blocker").style["display"] = "none";
+    for (let card of cards) {
+        card.classList.remove('open')
+    }
 }
 
 function init() {
-    let cards = document.getElementsByTagName("td");
-    shuffleCards(cards);
+    cards = document.getElementsByTagName("td");
     for (let card of cards) {
         card.onclick = () => onCardClick(card);
         card.style["visibility"] = "visible";
@@ -57,6 +91,9 @@ function init() {
                     openedCards[0].style["visibility"] = "hidden";
                     openedCards[1].style["visibility"] = "hidden";
                     score++;
+                    if (checkWinner()) {
+                        winner();
+                    }
                 }
                 for (let card of openedCards) {
                     card.classList.remove('open');
@@ -72,16 +109,16 @@ function init() {
         let value2 = card2.children[1].children[0].src;
         return value1 === value2;
     }
+}
 
-    function shuffleCards(cards) {
-        for (let i = 0; i < 500; i++) {
-            let cardIndex1 = Math.round(Math.random() * (cards.length - 1));
-            let cardIndex2 = Math.round(Math.random() * (cards.length - 1));
-            let card1 = cards[cardIndex1];
-            let card2 = cards[cardIndex2];
-            let imgTmp = card1.children[1].children[0].src;
-            card1.children[1].children[0].src = card2.children[1].children[0].src;
-            card2.children[1].children[0].src = imgTmp;
-        }
+function shuffleCards(cards) {
+    for (let i = 0; i < 500; i++) {
+        let cardIndex1 = Math.round(Math.random() * (cards.length - 1));
+        let cardIndex2 = Math.round(Math.random() * (cards.length - 1));
+        let card1 = cards[cardIndex1];
+        let card2 = cards[cardIndex2];
+        let imgTmp = card1.children[1].children[0].src;
+        card1.children[1].children[0].src = card2.children[1].children[0].src;
+        card2.children[1].children[0].src = imgTmp;
     }
 }
